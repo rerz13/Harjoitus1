cb7678863bc87285066bc5cff0a3aecddb13b7da
zarema.gasanova <zarema.gasanova@PC2EMQVM>
2025-09-18 10:05:15 +0300

    Add project files.

---
+++ Harj1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Harj1
+{
+    partial class VaihdaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OtsikkoLB = new System.Windows.Forms.Label();
+            this.VaihdaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OtsikkoLB
+            // 
+            this.OtsikkoLB.AutoSize = true;
+            this.OtsikkoLB.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OtsikkoLB.Location = new System.Drawing.Point(141, 54);
+            this.OtsikkoLB.Name = "OtsikkoLB";
+            this.OtsikkoLB.Size = new System.Drawing.Size(63, 21);
+            this.OtsikkoLB.TabIndex = 0;
+            this.OtsikkoLB.Text = "Otsikko";
+            // 
+            // VaihdaBT
+            // 
+            this.VaihdaBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VaihdaBT.Location = new System.Drawing.Point(115, 91);
+            this.VaihdaBT.Name = "VaihdaBT";
+            this.VaihdaBT.Size = new System.Drawing.Size(109, 61);
+            this.VaihdaBT.TabIndex = 1;
+            this.VaihdaBT.Text = "Vaihda teksti";
+            this.VaihdaBT.UseVisualStyleBackColor = true;
+            this.VaihdaBT.Click += new System.EventHandler(this.VaihdaBT_Click);
+            // 
+            // VaihdaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 247);
+            this.Controls.Add(this.VaihdaBT);
+            this.Controls.Add(this.OtsikkoLB);
+            this.Name = "VaihdaForm";
+            this.Text = "Vaihda teksti";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OtsikkoLB;
+        private System.Windows.Forms.Button VaihdaBT;
+    }
+}
+
---
+++ Harj1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harj1
+{
+    public partial class VaihdaForm : Form
+    {
+        public VaihdaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void VaihdaBT_Click(object sender, EventArgs e)
+        {
+            OtsikkoLB.Text = "Heippa maailma!";
+        }
+    }
+}
